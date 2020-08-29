6dfe9aa484c6162cf2080d3af3a97eb15a4c81df
aiivy782 <muntyanegorik@gmail.com>
2020-08-29 23:50:00 +0300

    vi

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "This script will take a long time to execute.  All programs have their own weight, so don't worry if the script folder is so heavy after installation."
+sleep 5s
+apt update && apt upgrade
+apt install git python python2 ruby-full
+pip install --upgrade pip
+git clone https://github.com/threat9/routersploit
+cd routersploit
+pip install -t requirements.txt
+chmod 777 rsf.py
+cd ..
+git clone https://github.com/rapid7/metasploit-framework
+cd metasploit
+chmod 777 msfconsole
+cd ..
+git clone https://github.com/Ignitetch/AdvPhishing
+cd AdvPhishing
+chmod 777 AnAdv-Phishing.sh
+cd ..
+echo "Installation finished! Check README.md for support.
+clear
