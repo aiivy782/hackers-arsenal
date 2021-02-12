0b78d73db58cee430a4947d31364a20a05d09aed
AIIVY <46345948+aiivy782@users.noreply.github.com>
2021-02-13 00:40:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "This script will take a long time to execute.  All programs have their own weight, so don't worry if the script folder is so heavy after installation."
 sleep 5s
-apt update && apt upgrade
-apt install git python python2 ruby-full
+apt update && apt upgrade -y
+apt install git python python2 ruby-full -y
 pip install --upgrade pip
 git clone https://github.com/threat9/routersploit
 cd routersploit
